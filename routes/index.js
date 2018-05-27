bd5f2b0f7888560f2441047fb72d2a3e839dc8c0
Ahmet  Kiziltan <ahmet@Ahmets-MacBook-Pro.local>
2018-05-27 18:17:59 +0300

    data bindings

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,39 @@ var router = express.Router();
 
 
 var fs = require('fs');
-var obj;
-fs.readFile('./data/data.json', 'utf8', function (err, data) {
+var companies;
+fs.readFile('./data/companies.json', 'utf8', function (err, data) {
   if (err) 
   	{
   		console.log(err);
   		throw err;
   	}
-  	
-  obj = JSON.parse(data);
-  console.log(obj);
+
+  companies = JSON.parse(data);
+  console.log(companies);
 });
+
+var polls;
+fs.readFile('./data/polls.json', 'utf8', function (err, data) {
+  if (err) 
+  	{
+  		console.log(err);
+  		throw err;
+  	}
+
+  polls = JSON.parse(data);
+  console.log(polls);
+});
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: 'Secim Anketleri',
-  	data: obj
+  	data: companies,
+  	recent_polls: polls
   });
 });
 
